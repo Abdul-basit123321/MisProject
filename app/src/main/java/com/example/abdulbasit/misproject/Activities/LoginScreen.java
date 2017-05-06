af16e86ab4ccb6a2984c55f6169663c3bb482cf5
Abdul basit <abdul.basit@siliconplex.com>
2017-05-06 15:21:10 +0500

    Login Screen

--- app/src/main/java/com/example/abdulbasit/misproject/Activities/LoginScreen.java
+++ app/src/main/java/com/example/abdulbasit/misproject/Activities/LoginScreen.java
@@ -1,5 +1,10 @@
 package com.example.abdulbasit.misproject.Activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.abdulbasit.misproject.R;
+
 /**
  * Created by Abdul basit on 5/6/2017.
  */
@@ -8,5 +13,11 @@ public class LoginScreen extends BaseActivity {
     @Override
     public int getDockFrameLayoutId() {
         return 0;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
     }
 }
