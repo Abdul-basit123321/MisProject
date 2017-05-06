ed1ca0d8ec9e804d36af4b7abc0559107f370df3
AsharAzam <asharazam@outlook.com>
2017-05-06 20:33:35 +0500

    Adapter done!

---
+++ app/src/main/java/com/example/abdulbasit/misproject/Adapters/CustomAdapter.java
@@ -0,0 +1,68 @@
+package com.example.abdulbasit.misproject.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.abdulbasit.misproject.Entities.Contact;
+import com.example.abdulbasit.misproject.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ashar on 5/6/2017.
+ */
+
+public class CustomAdapter extends ArrayAdapter<Contact>{
+    private ArrayList<Contact> dataSet;
+    Context mContext;
+
+    public CustomAdapter(ArrayList<Contact> data, Context context) {
+        super(context, R.layout.row_list, data);
+        this.dataSet = data;
+        this.mContext=context;
+
+    }
+
+    private static class ViewHolder {
+        TextView txtName;
+        TextView txtEmail;
+        TextView txtphone;
+        ImageView person_Image;
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Contact contact=getItem(position);
+        ViewHolder viewHolder;
+        final View result;
+        if (convertView==null)
+        {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater=LayoutInflater.from(getContext());
+            convertView=inflater.inflate(R.layout.row_list, parent, false);
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.person_name);
+            viewHolder.txtEmail = (TextView) convertView.findViewById(R.id.Email);
+            viewHolder.txtphone = (TextView) convertView.findViewById(R.id.phone_number);
+            viewHolder.person_Image = (ImageView) convertView.findViewById(R.id.Person_image);
+            result=convertView;
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+        viewHolder.txtName.setText(contact.getName());
+        viewHolder.txtEmail.setText(contact.getEmail());
+        viewHolder.txtphone.setText(contact.getNumber());
+        viewHolder.person_Image.setTag(position);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+
+
+}
